4b05183eaa169f00a0297a9b5a7c384d517e8f45
Shabina Singh <singh.shab@northeastern.edu>
2024-02-24 13:40:24 -0500

    edit

--- packer/db_init.sh
+++ packer/db_init.sh
@@ -12,7 +12,7 @@ PG_CONFIG=$(sudo -i -u postgres psql -t -P format=unaligned -c 'show hba_file';)
 echo "$PG_CONFIG"
 # PG_CONFIG="/var/lib/pgsql/data/pg_hba.conf"
 #echo "host    all             postgres        127.0.0.1/32            trust" | sudo tee -a "$PG_CONFIG"
-sudo -i -u postgres sed -i 's/ident/trust/g' "$PG_CONF"
+sudo -i -u postgres sed -i 's/ident/trust/g' "$PG_CONFIG"
 
 
 sudo tail "$PG_CONFIG"
